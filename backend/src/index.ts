ad37c606221cd58ff17ba47562e9dbf58c6b42c6
FossXpert <FossXpert@outlook.com>
2025-01-08 23:44:08 +0530

    update server URI in .env and remove unused CORS middleware from index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,13 +33,6 @@ app.use(
     })
 );
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins (you can restrict this later)
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
     cloudinary.v2.config({
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
